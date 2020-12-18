c00ca9f9bf2519032928f97fcc912e21cb76eb41
SileTao <sile@quartic.ai>
2020-12-18 18:30:31 -0500

    add param search

--- fit_diff.py
+++ fit_diff.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
+import seaborn as sns
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import GridSearchCV
 
 from config import path
 
@@ -18,10 +19,38 @@ def generate_xy(file_fitting_results, file_latest_combined_proc):
     return X, diff
 
 
-def fit_predict(X, diff):
-    rf = RandomForestRegressor(n_estimators=30, max_depth=2, max_features="sqrt", random_state=0)
-    rf.fit(X, diff)
-    return rf.predict(X), rf
+def search_opt_model(X, y):
+    param_grid = {'max_depth': [2, 3, 4, 5, 6, 7]}
+    rf = RandomForestRegressor(n_estimators=30, max_features="sqrt", random_state=0)
+    regressor = GridSearchCV(rf, param_grid, cv=10)
+    regressor.fit(X, y)
+    print(regressor.best_estimator_)
+    return regressor.best_estimator_
+
+
+def fit_predict(model, X, diff):
+    model.fit(X, diff)
+    return model.predict(X)
+
+
+def plot_pred2actual(actual, pred):
+    plt.scatter(actual, pred)
+    plt.axline([0, 0], [1, 1])
+    plt.axis('equal')
+    plt.xlabel("Difference")
+    plt.ylabel("Prediction")
+    plt.show()
+
+
+def plot_feature_importance(rf, X):
+    num_top_features = 10
+    feature_importances = pd.DataFrame(rf.feature_importances_, index=X.columns, columns=["Importance"])
+    feature_importances.sort_values(by='Importance', ascending=False, inplace=True)
+    feature_importances = feature_importances.iloc[:num_top_features, :]
+
+    sns.barplot(x=feature_importances["Importance"].values, y=feature_importances.index)
+    plt.title(f"Feature Importance (Top {num_top_features})")
+    plt.show()
 
 
 file_fitting_results = "data_fitting_results.csv"
@@ -29,20 +58,9 @@ file_latest_combined_proc = "OxCGRT_latest_combined_proc.csv"
 X, diff = generate_xy(file_fitting_results, file_latest_combined_proc)
 print("Shape of data: ", X.shape)
 
-pred, rf = fit_predict(X, diff)
+opt_rf = search_opt_model(X, diff)
+pred = fit_predict(opt_rf, X, diff)
 print("mean_squared_error: ", mean_squared_error(diff, pred))
 
-plt.scatter(diff, pred)
-plt.xlabel("Difference")
-plt.ylabel("Prediction")
-plt.show()
-
-features = X.columns.tolist()
-importances = rf.feature_importances_
-indices = np.argsort(importances)
-
-num_features = 5
-feat_importances = pd.DataFrame(rf.feature_importances_, index=X.columns, columns=["Importance"])
-feat_importances.sort_values(by='Importance', ascending=False, inplace=True)
-feat_importances[:num_features].plot(kind='bar', figsize=(8, 6), rot=20)
-plt.show()
+plot_pred2actual(diff, pred)
+plot_feature_importance(opt_rf, X)
