8c0e79fc348ed2244dac65006d7eab92b3a780b6
SileTao <sile@quartic.ai>
2020-12-29 14:15:19 -0500

    restructure and add tail peaks

--- peak_finding.py
+++ peak_finding.py
@@ -11,7 +11,7 @@ def get_smoothed_data(s, window_width):
     return s.rolling(window=window_width, center=True, min_periods=(window_width // 2)).mean()
 
 
-def make_plot(df_i, country):
+def make_plot(df_i, country, peaks, bottoms, cases_smoothed):
     dates = df_i["Date"].str.split("-", n=1, expand=True)[1]
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -34,17 +34,27 @@ def make_plot(df_i, country):
     plt.close()
 
 
+def get_tail_peaks(cases_smoothed, peaks, tail_length=10):
+    cases_smoothed_tail = cases_smoothed[-tail_length:]
+    if sum(cases_smoothed_tail.diff() > 0) > (tail_length / 2):
+        peaks = np.append(peaks, len(cases_smoothed) - 1)
+    return peaks
+
+
+def process(df, height_threshold=0.20, prominence_threshold=0.10, distance=60, window_width=28):
+    for country in tqdm(df["Entity"].unique()):
+        df_i = df[(df["Entity"] == country) & (df["Days_30"] >= 0)]
+
+        if not df_i.empty:
+            cases_smoothed = get_smoothed_data(df_i["cases"], window_width=window_width)
+            peaks, _ = find_peaks(cases_smoothed, distance=distance,
+                                  height=np.quantile(df_i["cases"].round().unique(), height_threshold),
+                                  prominence=np.quantile(df_i["cases"].round().unique(), prominence_threshold))
+            bottoms, _ = find_peaks((-1) * cases_smoothed, distance=distance,
+                                    prominence=np.quantile(df_i["cases"].round().unique(), prominence_threshold))
+            peaks = get_tail_peaks(cases_smoothed, peaks)
+            make_plot(df_i, country, peaks, bottoms, cases_smoothed)
+
+
 df = pd.read_csv(in_path + "cases.csv")
-
-for country in tqdm(df["Entity"].unique()):
-    df_i = df[(df["Entity"] == country) & (df["Days_30"] >= 0)]
-
-    if not df_i.empty:
-        cases_smoothed = get_smoothed_data(df_i["cases"], window_width=28)
-        peaks, _ = find_peaks(cases_smoothed, distance=60, height=np.quantile(df_i["cases"].round().unique(), 0.20),
-                              prominence=np.quantile(df_i["cases"].round().unique(), 0.20))
-        bottoms, _ = find_peaks((-1) * cases_smoothed, distance=60,
-                                prominence=np.quantile(df_i["cases"].round().unique(), 0.20))
-        if cases_smoothed.values[len(cases_smoothed) - 1] > np.quantile(cases_smoothed.round().unique(), 0.98):
-            peaks = np.append(peaks, len(cases_smoothed) - 1)
-        make_plot(df_i, country)
+process(df)
